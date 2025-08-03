1f82091b28f40fc57e4b40933b5da3e5b7c7b303
増田陽一 <35191636+mjXmsdgh@users.noreply.github.com>
2025-08-03 20:15:11 +0900

    Add time tracking and center index calculation
    Introduces a 'time' variable that accumulates delta each frame and calculates the center index of the grid in the _process function. These additions prepare for future features that may require time-based updates or operations centered on the grid.

--- script/FDTDSimulator.gd
+++ script/FDTDSimulator.gd
@@ -4,6 +4,8 @@ extends Node2D
 const GRID_WIDTH = 512  # グリッドの幅
 const GRID_HEIGHT = 512 # グリッドの高さ
 
+var time = 0.0
+
 # FDTD法で使用する物理量を格納する配列
 # PackedFloat32Arrayは高速な浮動小数点数配列
 var ez: PackedFloat32Array = PackedFloat32Array() # 電場 (Ez成分)
@@ -19,6 +21,9 @@ func _ready():
 
 
 func _process(delta):
+
+	time += delta
+
 	# Step A: 現在の電場(ez)を使って、次の瞬間の磁場(hx, hy)を計算
 	for y in range(1, GRID_HEIGHT - 1):
 		for x in range(1, GRID_WIDTH - 1):
@@ -35,3 +40,7 @@ func _process(delta):
 			var idx = y * GRID_WIDTH + x
 			ez[idx] += (hy[idx] - hy[idx - 1]) - (hx[idx] - hx[idx - GRID_WIDTH]) # Hx,Hyの変化からEzを計算
 
+
+	var center_x = GRID_WIDTH / 2
+	var center_y = GRID_HEIGHT / 2
+	var center_idx = center_y * GRID_WIDTH + center_x
